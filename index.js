57a85f7f1c6fe21b5cdf59a99afdb69a166c377e
emmeiwhite <emmeiwhite@gmail.com>
2025-07-10 22:49:51 +0530

    get requests to serve test users and products

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const express = require('express')
+import express from 'express'
 
 const app = express()
 
@@ -6,8 +6,26 @@ const PORT = process.env.PORT | 3000
 
 // 1. test route
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (request, response) => {
+  response.status(201).json({
+    message: 'resource created'
+  })
+})
+
+// Routes Practice
+
+app.get('/api/users', (request, response) => {
+  response.json([
+    { id: 1, name: 'Anson' },
+    { id: 2, name: 'Rafs' }
+  ])
+})
+
+app.get('/api/products', (request, response) => {
+  response.json([
+    { id: 1, name: 'chicken', price: 2, quantity: 1 },
+    { id: 2, name: 'fish', price: 1, quantity: 4 }
+  ])
 })
 
 app.listen(PORT, () => {
