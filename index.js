86ba712f1c69ad16d8a4a17313101f014ce455f7
emmeiwhite <emmeiwhite@gmail.com>
2025-07-10 23:06:46 +0530

    dynamic-route practice /:productId

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express()
 
 const PORT = process.env.PORT | 3000
 
+const mockProducts = [
+  { id: 1, name: 'chicken', price: 2, quantity: 1 },
+  { id: 2, name: 'fish', price: 1, quantity: 4 }
+]
 // 1. test route
 
 app.get('/', (request, response) => {
@@ -26,6 +30,23 @@ app.get('/api/products', (request, response) => {
     { id: 1, name: 'chicken', price: 2, quantity: 1 },
     { id: 2, name: 'fish', price: 1, quantity: 4 }
   ])
+})
+
+// Route Parameter: We pass in a  dynamic value in our route path (/:id e.g.,) and then the server would receive that request , it will check what the route parameter is, and then it will get the correct Entity (product, user etc) from the database
+
+app.get('/api/products/:productId', (request, response) => {
+  const { productId } = request.params
+
+  const resProduct = mockProducts.find(product => product.id === parseInt(productId))
+  console.log(resProduct)
+
+  if (!resProduct) {
+    return response.status(404).json({
+      message: 'Not Found'
+    })
+  }
+
+  response.status(200).json(resProduct)
 })
 
 app.listen(PORT, () => {
