d32e3b639ff741be08e6d85e86f7ea76b04b4add
emmeiwhite <emmeiwhite@gmail.com>
2025-07-11 14:53:35 +0530

    2. Middleware Global

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 import express from 'express'
 const app = express()
-
 const PORT = process.env.PORT || 3000
+
+app.use((request, response, next) => {
+  console.log(`logger: ${request.url} at ${Date.now()}`)
+  next()
+})
+
+// Home Route
+app.use('/', (request, response) => {
+  response.json({
+    message: 'Home Route'
+  })
+})
 
 app.listen(PORT, () => {
   console.log(`Running on Port: ${PORT}`)
