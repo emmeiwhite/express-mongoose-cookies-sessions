cc018ac7d6a9a804fae81b0351cd94a83937e30a
emmeiwhite <emmeiwhite@gmail.com>
2025-07-12 23:06:41 +0530

    cookie-2 clear-cookie

--- index.js
+++ index.js
@@ -8,6 +8,17 @@ app.get('/', (request, response) => {
   response.cookie('cookie1', 'eat and enjoy')
   response.send('Express Home')
 })
+
+app.get('/fetch', (request, response) => {
+  console.log(request.cookies)
+  response.send('API Data Fetched!')
+})
+
+app.get('/clear-cookie', (request, response) => {
+  response.clearCookie('cookie1')
+  response.send('Cookie cleared!')
+})
+
 app.listen(3000, () => {
   console.log('Server is listening on port 3000')
 })
