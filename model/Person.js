3128b748ce78a6d82573d21a090646b467f91d22
emmeiwhite <emmeiwhite@gmail.com>
2025-07-12 06:36:47 +0530

    model-5 Models covered to some extend

--- model/Person.js
+++ model/Person.js
@@ -1,12 +1,15 @@
 import mongoose from 'mongoose'
 
 // Schema defines the structure of our document inside the MongoDB collection
-const personSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  age: { type: Number, required: true },
-  email: { type: String, required: true, unique: true },
-  createdAt: { type: Date, default: Date.now() }
-})
+const personSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    age: { type: Number, required: true },
+    email: { type: String, required: true, unique: true },
+    userOrder: { type: Object, default: {} }
+  },
+  { timestamps: true, minimize: false }
+)
 
 // Create Modal from Schema with model constructor function to interact with the DB
 
