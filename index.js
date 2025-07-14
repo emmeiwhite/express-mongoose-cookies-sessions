4d822fb29443f33f8ed3c1e98b589fd12a05bbea
emmeiwhite <emmeiwhite@gmail.com>
2025-07-15 00:16:50 +0530

    JWT-6 Bug fixed in POST /login route with how to find user

--- index.js
+++ index.js
@@ -16,10 +16,38 @@ app.post('/register', async (req, res) => {
 
   // let's encrypt the password before storing into the database
   const hashedPassword = await bcrypt.hash(password, 10)
+
   users.push({ email, password: hashedPassword })
+
+  console.log(users)
   res.json({
     message: `User with email ${email} registered`
   })
+})
+
+/** --- 2. Let's now work on login Route --- */
+app.post('/login', async (req, res) => {
+  const { email, password } = req.body
+
+  // 🔍 Step 1: Find user by email
+  const currentUser = users.find(user => user.email === email)
+
+  if (!currentUser) {
+    return res.status(404).json({
+      message: `User with email ${email} not found`
+    })
+  }
+
+  // 🔐 Step 2: Compare password using bcrypt
+  const isMatch = await bcrypt.compare(password, currentUser.password)
+
+  if (!isMatch) {
+    return res.status(401).json({
+      message: `password is incorrect, please try again`
+    })
+  }
+
+  // If gmail and password are correct, let's create a JWT token for the user
 })
 
 app.listen(3000, (req, res) => {
