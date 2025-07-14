40a91ba333322f766c12dc85c2272fde403bc49f
emmeiwhite <emmeiwhite@gmail.com>
2025-07-14 23:55:18 +0530

    JWT-4 Mocking user registration

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 import express from 'express'
 const app = express()
 
+app.use(express.json())
+// Mock DB for now:
+const users = []
+
 app.get('/', (req, res) => {
   res.send('Home Route')
+})
+
+/** 1. Register User | req.body and save contents to the users array */
+app.post('/register', (req, res) => {
+  const { email, password } = req.body
+
+  users.push({ email, password })
+  res.json({
+    message: `User with email ${email} registered`
+  })
 })
 
 app.listen(3000, (req, res) => {
